e75b5e8ad21a830235f81a9713acc0625fb0187e
zj1926 <zj1926@users.noreply.github.com>
2017-11-29 13:08:40 -0500

    Update createObservable.js

--- src/createObservable.js
+++ src/createObservable.js
@@ -1,17 +1,34 @@
 
 // create
 var oneObservable = Rx.Observable.create(function subscribe(observer) {
-  var id = setInterval(() => {
-    observer.next('hi')
-  }, 1000);
+  try {
+    observer.next(1);
+    observer.next(2);
+    observer.next(3);
+    observer.complete();
+  } catch (err) {
+    observer.error(err); // delivers an error if it caught one
+  }
 });
 
 
-// Subscribing to Observables, analogous to function.call
-oneObservable.subscribe(x => console.log(x));
-// another way to subscribe
-oneObservable.subscribe({
-  next: x => console.log('got value ' + x),
-  error: err => console.error('something wrong occurred: ' + err),
-  complete: () => console.log('done'),
-});
+// Subscribing to Observables, analogous to function.call, using func as arguments
+var subscription1 = oneObservable.subscribe(x => console.log(x));
+var subscription2 = observable.subscribe(
+  x => console.log('Observer got a next value: ' + x),
+  err => console.error('Observer got an error: ' + err),
+  () => console.log('Observer got a complete notification')
+);
+
+
+// another way to subscribe with observer, with json
+var observer = {
+  next: x => console.log('Observer got a next value: ' + x),
+  error: err => console.error('Observer got an error: ' + err),
+  complete: () => console.log('Observer got a complete notification'),
+};
+var subscription3 = oneObservable.subscribe(observer);
+
+
+// unsubscribe
+subscription.unsubscribe();
