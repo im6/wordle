41fd34e5c79c2b683baed5ab438d82c0f356a2f9
im6 <ee2win@gmail.com>
2019-02-16 23:55:44 -0500

    add start btn

--- src/snake/main.js
+++ src/snake/main.js
@@ -39,50 +39,51 @@ import {
 import SnakeCanvas from './SnakeCanvas';
 
 const textElem = document.getElementById('scoreText');
+const startBtnElem = document.getElementById('gameStartBtn');
 const viewElem = new SnakeCanvas(document.getElementById('appCan'), textElem);
 
-const createGame = (animObs) => {
-  const direction$ = fromEvent(document, 'keydown').pipe(
-    map(({ keyCode }) => DIRECTIONS[keyCode]),
-    startWith(INIT_DIRECTION),
-    filter(d => !!d), // ignore other keydown
-    distinctUntilChanged(), // change on curve
-  );
-  
-  const len$ = new BehaviorSubject(SNAKE_INIT_LENGTH);
-  const snakeLen$ = len$.pipe(
-    scan((prev, next) => prev + next), // do this because $appleEaten release 1 every eating time
-  );
-  const score$ = snakeLen$.pipe(
-    startWith(0),
-    scan((prev, next) => prev + 1),
-  );
-  
-  const ticks$ = interval(GAME_INTERVAL);
-  const snake$ = ticks$.pipe(
+const startBtn$ = fromEvent(startBtnElem, 'click');
+
+const direction$ = fromEvent(document, 'keydown').pipe(
+  map(({ keyCode }) => DIRECTIONS[keyCode]),
+  startWith(INIT_DIRECTION),
+  filter(d => !!d), // ignore other keydown
+  distinctUntilChanged(), // change on curve
+);
+
+const len$ = new BehaviorSubject(SNAKE_INIT_LENGTH);
+const snakeLen$ = len$.pipe(
+  scan((prev, next) => prev + next), // do this because $appleEaten release 1 every eating time
+);
+const score$ = snakeLen$.pipe(
+  startWith(0),
+  scan((prev, next) => prev + 1),
+);
+
+const ticks$ = interval(GAME_INTERVAL);
+const snake$ = ticks$.pipe(
     withLatestFrom(direction$, snakeLen$, (_, direction, snakeLength) => [direction, snakeLength]), // mapper is optional but better to have, filter out unused.
     scan(move, initSnake()),
-    share());
-  
-  const apple$ = snake$.pipe(
-    scan(eat, SnakeCanvas.getRandomPosition()),
-    distinctUntilChanged(),
     share(),
   );
 
-  const appleEatenSubscription = apple$.pipe(
-    skip(1),
-    tap(() => len$.next(1))
-  ).subscribe();
-  
-  const scene$ = combineLatest(snake$, apple$, score$, (snake, apple, score) => ({ snake, apple, score, }));
-  
-  return animObs.pipe(withLatestFrom(scene$, (_, b) => b))
-}
+const apple$ = snake$.pipe(
+  scan(eat, SnakeCanvas.getRandomPosition()),
+  distinctUntilChanged(),
+  share(),
+);
 
+const appleEatenSubscription = apple$.pipe(
+  skip(1),
+  tap(() => len$.next(1))
+).subscribe();
+
+const scene$ = combineLatest(snake$, apple$, score$, (snake, apple, score) => ({ snake, apple, score,}));
 const game$ = of('Start Game').pipe(
   map(_ => interval(1000 / 10, animationFrameScheduler)),
-  switchMap(createGame), // from 'Start Game' to game scene observable
+  switchMap(anim => anim.pipe(
+    withLatestFrom(scene$, (_, scene) => scene),
+  )), // from 'Start Game' to game scene observable
   takeWhile(willHitSelf),
 );
 
@@ -95,3 +96,15 @@ game$.subscribe({
     textElem.innerText = 'game over';
   }
 });
+
+startBtn$.subscribe({
+  next: e =>{
+    len$.next(1);
+    
+    const newGame = {
+      apple: SnakeCanvas.getRandomPosition(),
+      score: 1,
+      snake: initSnake(),
+    };
+  }
+})
